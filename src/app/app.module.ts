e2a677ea2145c19d017fd858b1d9be4efa52a37e
bwsimiyu <briansimiyu@icloud.com>
2021-04-28 15:34:50 +0300

    Revamping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialImports } from './imports/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ...MaterialImports
   ],
   providers: [],
   bootstrap: [AppComponent]
